3160ee6ff33bc24b4d3138ef10e43f8988c28476
Francisco Ceruti <hello@fceruti.com>
2019-04-29 12:30:33 +0545

    Add flake8

--- apps/misc/hello_universe_urls.py
+++ apps/misc/hello_universe_urls.py
@@ -5,6 +5,7 @@ from django.views.generic import TemplateView
 class HelloUniverseView(TemplateView):
     template_name = 'misc/hello_universe.html'
 
+
 urlpatterns = [
     path('', HelloUniverseView.as_view()),
 ]

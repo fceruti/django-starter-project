6f1920dce5fc81871e7bf904ed3983f69e99484a
Francisco Ceruti <hello@fceruti.com>
2019-05-21 16:06:52 +0300

    Add TimestampedModel and CodeModel

--- apps/misc/models.py
+++ apps/misc/models.py
@@ -1,4 +1,7 @@
+import uuid
 from collections import Callable
+
+from django.db import models
 
 
 class Choices:
@@ -32,3 +35,40 @@ class Choices:
     @classmethod
     def keys(cls):
         return [choice[0] for choice in cls.choices()]
+
+
+class TimestampedModel(models.Model):
+    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        abstract = True
+
+
+class CodeModel(TimestampedModel):
+    code = models.CharField(
+        max_length=255,
+        unique=True,
+        db_index=True,
+        null=False,
+        blank=True)
+
+    class Meta:
+        abstract = True
+
+    @property
+    def short_code(self):
+        return self.code[:8]
+
+    def save(self, *args, **kwargs):
+        if not self.code:
+            while True:
+                new_code = str(uuid.uuid4())
+                try:
+                    self.__class__.objects.get(code=code)
+                    self.code = new_code
+                    break
+                except self.__class__.DoesNotExist:
+                    continue
+            self.code = new_code
+        super().save(*args, **kwargs)

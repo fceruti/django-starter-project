233ebca6d0ab54235731dd44ec76e4ae094eae38
Francisco Ceruti <hello@fceruti.com>
2019-05-27 17:56:50 +0300

    Create docker volumes of logs and media

--- conf/urls.py
+++ conf/urls.py
@@ -1,4 +1,5 @@
 from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
@@ -7,6 +8,12 @@ urlpatterns = [
     path('', include('apps.users.urls.auth')),
     path('admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(
+        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.USE_DEBUG_TOOLBAR:
     import debug_toolbar

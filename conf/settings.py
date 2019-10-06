04ea10a8c9639aaa6d4846d2740ba7b515609f1d
Francisco Ceruti <hello@fceruti.com>
2019-10-06 16:46:12 +0300

    Add django-storages

--- conf/settings.py
+++ conf/settings.py
@@ -150,6 +150,21 @@ WEBPACK_LOADER = {
     }
 }
 
+USE_S3_STATIC_STORAGE = env.bool('USE_S3_STATIC_STORAGE', default=False)
+
+if USE_S3_STATIC_STORAGE:
+    AWS_ACCESS_KEY_ID = env('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = env('AWS_SECRET_ACCESS_KEY')
+    AWS_STORAGE_BUCKET_NAME = env('AWS_STORAGE_BUCKET_NAME')
+    AWS_S3_ENDPOINT_URL = env('AWS_S3_ENDPOINT_URL')
+    AWS_S3_OBJECT_PARAMETERS = {
+        'CacheControl': 'max-age=86400',
+    }
+    AWS_LOCATION = 'static'
+    AWS_DEFAULT_ACL = 'public-read'
+    STATIC_URL = f'{AWS_S3_ENDPOINT_URL}/{AWS_LOCATION}/'
+    STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
 # -----------------------------------------------------------------------------
 # Django Debug Toolbar
 # -----------------------------------------------------------------------------
